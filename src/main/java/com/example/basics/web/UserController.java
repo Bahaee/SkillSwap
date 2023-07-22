4cd57032af4e871b000fde77beefde3545c1afb2
Bahae <bahaeddine_moutaouikil@um5.ac.ma>
2023-07-22 16:22:42 +0000

    Add test web service

--- src/main/java/com/example/basics/web/UserController.java
+++ src/main/java/com/example/basics/web/UserController.java
@@ -2,6 +2,7 @@ package com.example.basics.web;
 
 
 import com.example.basics.entities.User;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -10,12 +11,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.example.basics.repository.UserRepository;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UserController {
 
   @Autowired
   private UserRepository userRepository;
+
+  @GetMapping("/users")
+  public List<User> users(){
+    return userRepository.findAll();
+  }
 
   @GetMapping("/index")
   public String index(Model model,
